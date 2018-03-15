cb6ac4b7258bbf1fe27974fd25ba4aa785754557
l.yang <liang.yang@advance.ai>
2018-03-15 17:03:53 +0800

    change assert

--- src/test/java/org/gorgeous/fitness/util/SpringBeanHelperTest.java
+++ src/test/java/org/gorgeous/fitness/util/SpringBeanHelperTest.java
@@ -2,15 +2,14 @@ package org.gorgeous.fitness.util;
 
 import org.gorgeous.AbstractTest;
 import org.gorgeous.fitness.domain.mapper.AreaMapper;
+import org.junit.Assert;
 import org.junit.Test;
-
-import static org.junit.Assert.*;
 
 public class SpringBeanHelperTest extends AbstractTest {
 
     @Test
     public void getBean() {
         AreaMapper mapper = SpringBeanHelper.getBean(AreaMapper.class);
-        assert mapper != null;
+        Assert.assertNotNull(mapper);
     }
 }
