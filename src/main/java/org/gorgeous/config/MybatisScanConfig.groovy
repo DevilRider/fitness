d152ded8feb98200f728da8d28a3a846be536bf9
l.yang <liang.yang@advance.ai>
2018-03-15 11:30:47 +0800

    add db settings

---
+++ src/main/java/org/gorgeous/config/MybatisScanConfig.groovy
@@ -0,0 +1,16 @@
+package org.gorgeous.config
+
+import groovy.transform.CompileStatic
+import org.mybatis.spring.annotation.MapperScan
+import org.springframework.context.annotation.Configuration
+
+/**
+ * Mybatis 配置.
+ * @project fitness
+ * @author l.yang on 2018/3/15 上午11:07
+ */
+@Configuration
+@CompileStatic
+@MapperScan(basePackages = ['org.fitness.*.mapper'], sqlSessionTemplateRef = "sqlSession")
+class MybatisScanConfig {
+}
